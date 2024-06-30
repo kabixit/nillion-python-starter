01a9b77b3c61e997e66f7bea8d987e4943324942
kabixit <kumarakabilan2004@gmail.com>
2024-06-30 18:02:14 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,22 +1,9 @@
 from nada_dsl import *
 
 def nada_main():
-    prayasu = Party(name="Prayasu")  # party 0
-    datta = Party(name="Datta")      # party 1
-
-    prayasu_skill = SecretInteger(Input(name="prayasu_skill", party=prayasu))
-    datta_skill = SecretInteger(Input(name="datta_skill", party=datta))
-
-    selection_result = (
-        (prayasu_skill > datta_skill).if_else(
-            Integer(0),
-            (datta_skill > prayasu_skill).if_else(
-                Integer(1),
-                Integer(2)
-            )
-        )
-    )
-
-    out = Output(selection_result, "selection_result", prayasu)
-
-    return [out]
+    nillion = Party(name="nillion")
+    x = SecretInteger(Input(name="x", party=nillion))
+    y = SecretInteger(Input(name="y", party=nillion))
+    condition = x > y
+    maximum = condition.if_else(x, y)
+    return [Output(maximum, "maximum", nillion)]
